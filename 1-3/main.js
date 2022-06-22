774fab12cbb753172eaab22e7eb6825452933cb2
okazakitemma <107670740+okazakitemma@users.noreply.github.com>
2022-06-22 21:58:21 +0900

    1-3課題提出
    条件分岐演算子課題

---
+++ 1-3/main.js
@@ -0,0 +1,19 @@
+let a = 12;
+console.log(a != 10);
+
+let b = 10;
+console.log(b >= 10 && b < 20 && b % 2 === 0);
+
+let x = 10;
+if (x >= 10 && x <= 20) {
+    console.log('成功です');
+} else {
+    console.log('失敗です');
+}
+
+let y = 0;
+if (y % 2 === 0) {
+    console.log('偶数です');
+} else if (y % 2 !== 0) {
+    console.log('奇数です');
+}
